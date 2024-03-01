3bff81bedba83d8e4469479768dce7bc867f8339
Myhailo Riznyk <myhailo@MBP-Myhailo.lan>
2024-03-01 13:42:45 +0200

    m

--- tracker.py
+++ tracker.py
@@ -21,30 +21,31 @@ async def send_message_with_delay(chat_id: int, message: str):
 async def check_manager_delay(message: types.Message):
     global last_message
     last_message = message
-   
+    
     user_id = message.from_user.id
     user = session.query(User).filter_by(id = user_id).first()
     chat = session.query(Chat).filter_by(chat_id = message.chat.shifted_id).first()
     if not user and chat:
-        await asyncio.sleep(10)
-        if last_message.message_id == message.message_id:
-            manager = session.query(User).filter_by(team_id = chat.team_id, role = 'Афф-менеджер').first()
-            await last_message.reply(f"Из-за высокой загруженности время ответа менеджера увеличивается, просим немного Вашего терпения! {manager.name}")
-            if manager.paused < datetime.now():
-                await remove_score(manager.id, 1)
-            await asyncio.sleep(10)
+        if message.text.endswith('?'):
+            await asyncio.sleep(3600)
             if last_message.message_id == message.message_id:
-                team_lead = session.query(User).filter_by(team_id = manager.team_id, role = 'Тимлид').first()
-                await last_message.reply(f"Приносим извинения за задержку, скоро будет ответ {team_lead.name} {manager.name}")
+                manager = session.query(User).filter_by(team_id = chat.team_id, role = 'Афф-менеджер').first()
+                await last_message.reply(f"Из-за высокой загруженности время ответа менеджера увеличивается, просим немного Вашего терпения! {manager.name}")
                 if manager.paused < datetime.now():
                     await remove_score(manager.id, 1)
-                await asyncio.sleep(10)
+                await asyncio.sleep(1800)
                 if last_message.message_id == message.message_id:
+                    team_lead = session.query(User).filter_by(team_id = manager.team_id, role = 'Тимлид').first()
+                    await last_message.reply(f"Приносим извинения за задержку, скоро будет ответ {team_lead.name} {manager.name}")
                     if manager.paused < datetime.now():
-                        await remove_score(manager.id, 5)
-                    if team_lead.paused < datetime.now():
-                        await remove_score(team_lead.id, 3)
-                    await last_message.reply(f"Приносим извинения за задержку {team_lead.name} {manager.name} {head}")
+                        await remove_score(manager.id, 1)
+                    await asyncio.sleep(3600)
+                    if last_message.message_id == message.message_id:
+                        if manager.paused < datetime.now():
+                            await remove_score(manager.id, 5)
+                        if team_lead.paused < datetime.now():
+                            await remove_score(team_lead.id, 3)
+                        await last_message.reply(f"Приносим извинения за задержку {team_lead.name} {manager.name} {head}")
 
 async def remove_score(user_id: int, score: int):
     user = session.query(User).filter_by(id=user_id).first()
@@ -76,5 +77,5 @@ def calculate_average_reply_time(message: types.Message):
 
 
 def register_tracker(dp: Dispatcher):
-    dp.register_message_handler(check_manager_delay, lambda m: m is not None and m.text.endswith('?') and m.chat.type in ('group', 'supergroup'), content_types = types.ContentTypes.TEXT | types.ContentTypes.PHOTO | types.ContentTypes.VIDEO, state="*")
+    dp.register_message_handler(check_manager_delay, lambda m: m is not None and m.chat.type in ('group', 'supergroup'), content_types = types.ContentTypes.TEXT | types.ContentTypes.PHOTO | types.ContentTypes.VIDEO, state="*")
     dp.register_message_handler(calculate_average_reply_time, lambda m: m.reply_to_message is not None, content_types = types.ContentType.ANY, state="*")
