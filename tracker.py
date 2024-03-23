92545dae958de1036a8e8a5bf10a95dfad8090e0
Myhailo Riznyk <myhailo@MBP-Myhailo.lan>
2024-03-23 17:41:16 +0200

    m

--- tracker.py
+++ tracker.py
@@ -79,7 +79,7 @@ async def check_manager_delay(message: types.Message):
                                             await last_message.reply(f"Приносим извинения за задержку {team_lead.name} {head}")
                         
             elif user and chat:
-                if not session.query(User).filter_by(id = last_message.from_id).first() and '?' in last_message.text:
+                if not session.query(User).filter_by(id = last_message.from_id).first() and '?' in last_message.text and user.role in ('Тимлид', 'Афф-менеджер'):
                     print('calculated')
                     calculate_average_reply_time(last_message, message)
 
