83d0e79bdcbcfacde6370fd2d4309364f0107244
Myhailo Riznyk <myhailo@MacBook-Pro-Myhailo.local>
2024-04-08 14:01:05 +0300

    m

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 head = "@Danil_Cataffs"
-admins = ["m1i1ha"]
-admin_ids = [1095610815]#6653984089
+admins = ["m1i1ha", 'Viktoria_cataffs']
+admin_ids = [1095610815, 6653984089]
 
 TOKEN = "7059450347:AAFkpI7q1SAPPPjqXE738EfBhB0J7ByRJTs"
 bot = Bot(token = TOKEN)
