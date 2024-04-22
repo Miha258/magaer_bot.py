e017a29b24e26ed3898a66dc85190e5a9bae5a13
Myhailo Riznyk <myhailo@MBP-Myhailo.lan>
2024-04-22 16:31:59 +0300

    m

--- manager.py
+++ manager.py
@@ -2,6 +2,7 @@ from aiogram import types
 from config import *
 from utils import *
 from db import session, User
+from math import floor
 
 async def show_personal_statistics(message: types.Message):
     user_id = message.from_user.id
@@ -9,12 +10,15 @@ async def show_personal_statistics(message: types.Message):
     if user:
         avrg_worktime = user.average_reply_worktime / 60 if user.average_reply_worktime else 0 
         avrg_time = user.average_reply_time / 60 if user.average_reply_time else 0
+     
+        avrg_worktime_secs = int(avrg_worktime % floor(avrg_worktime) * 100)
+        avrg_time_secs = int(avrg_time % floor(avrg_time) * 100)
         response = f"<strong>Личная статистика:</strong>\n\n"
         response += f"<strong>Количество баллов:</strong> {user.quality_score}\n"
         response += f"<strong>Начало рабочего времени:</strong> {':'.join(str(user.start_work_at).split(':')[:-1])}\n"
         response += f"<strong>Конец рабочего времени:</strong> {':'.join(str(user.end_work_at).split(':')[:-1])}\n"
-        response += f"<strong>Среднее время ответа в рабочее время:</strong> {avrg_worktime:.2f} мин.\n"
-        response += f"<strong>Среднее время ответа в нерабочее время:</strong> {avrg_time:.2f} мин.\n"
+        response += f"<strong>Среднее время ответа в рабочее время:</strong> {int(avrg_worktime)} мин. {avrg_worktime_secs}cек.\n"
+        response += f"<strong>Среднее время ответа в нерабочее время:</strong> {int(avrg_time)} мин. {avrg_time_secs} cек.\n"
         if user.paused > datetime.now():
             response += f'<strong>Перерыв до: </strong>" + {datetime.strftime(user.paused, "%Y.%m.%d-%H:%M")}'
         await message.answer(response, parse_mode='html')
